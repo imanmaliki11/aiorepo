3b1a18583d984eed5e68cf9088ab6d4ef3198558
Iman Maliki <45257900+imanmaliki11@users.noreply.github.com>
2020-10-25 04:46:40 +0700

    Update iman.js

--- iman.js
+++ iman.js
@@ -5,38 +5,56 @@
 // @grant none
 // ==/UserScript==
 
-var s1 = false, s2=false, s3=false;
+var s0=false, s1 = false, s2=false, s3=false;
 
-var iman = setInterval(run, 500);
+var iman = setInterval(run, 50);
 
 //var i = setTimeout(run, 1000);
 
 function run() {
   
-  if(!s1) {
-      console.log('======= 1 ========');
-      var x = document.getElementsByClassName("_2O0llP")[0].lastElementChild;
-      if(x) {
-        x.click();
-        s1 = true;
+  var tt = new Date(Date.UTC('2020','09','24','17','00','00'));
+  var d = new Date();
+  var n = d.getTime();
+  var x = tt.getTime();
+  
+  if(x <= d) {
+  
+    if(!s0) {
+        console.log('======= 0 ========');
+        var r = document.getElementsByClassName("crl7WW")[0].firstElementChild;
+        if(r) {
+          r.click();
+          s0 = true;
+        }
+    }
+    
+    if(!s1) {
+        console.log('======= 1 ========');
+        var x = document.getElementsByClassName("_2O0llP")[0].lastElementChild;
+        if(x) {
+          x.click();
+          s1 = true;
+        }
+    }
+
+    if(!s2) {
+      console.log('======= 2 ========');
+      var y = document.querySelector(".cart-page-footer__checkout button");
+      if(y) {
+        y.click()
+        s2 = true;
       }
-  }
-  
-  if(!s2) {
-    console.log('======= 2 ========');
-    var y = document.querySelector(".cart-page-footer__checkout button");
-    if(y) {
-      y.click()
-      s2 = true;
     }
-  }
-  
-  if(!s3) {
-    console.log('======= 3 ========');
-    var z = document.querySelector("._1WpGLP button");
-    if(z.click()) {
-      s3 = true;
+
+    if(!s3) {
+      console.log('======= 3 ========');
+      var z = document.querySelector("._1WpGLP button");
+      if(z.click()) {
+        s3 = true;
+      }
     }
+    
   }
   
   
