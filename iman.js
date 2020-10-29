9cce81bf6ae58b5c544ab1dfa3e21ebd16f8e836
Iman Maliki <45257900+imanmaliki11@users.noreply.github.com>
2020-10-29 15:05:46 +0700

    Fix issue num item

--- iman.js
+++ iman.js
@@ -6,55 +6,97 @@
 // ==/UserScript==
 
 var s0=false, s1 = false, s2=false, s3=false;
+var c1=0, c2=0, c3=0;
 
 var iman = setInterval(run, 50);
 
-//var i = setTimeout(run, 1000);
-
 function run() {
   
-  var tt = new Date(Date.UTC('2020','09','24','17','00','00'));
+  var tt = new Date(Date.UTC('2020','09','29','06','00','00'));
   var d = new Date();
   var n = d.getTime();
   var x = tt.getTime();
   
   if(x <= d) {
   
-    if(!s0) {
+    try {
+      if(!s0) {
         console.log('======= 0 ========');
         var r = document.getElementsByClassName("crl7WW")[0].firstElementChild;
-        if(r) {
-          r.click();
+        if(r.click()) {
           s0 = true;
         }
+      }
+    } catch {
+      console.log("------ G 0 --");
     }
     
-    if(!s1) {
+    try {
+      if(!s1) {
         console.log('======= 1 ========');
         var x = document.getElementsByClassName("_2O0llP")[0].lastElementChild;
         if(x) {
           x.click();
           s1 = true;
+          c1=0;
+        } else if(s0) {
+          if(c1 > 20) {
+            s0 = true;
+            c1=0;
+          } else{
+            c1++;
+          }
         }
-    }
-
-    if(!s2) {
-      console.log('======= 2 ========');
-      var y = document.querySelector(".cart-page-footer__checkout button");
-      if(y) {
-        y.click()
-        s2 = true;
       }
-    }
-
-    if(!s3) {
-      console.log('======= 3 ========');
-      var z = document.querySelector("._1WpGLP button");
-      if(z.click()) {
-        s3 = true;
-      }
+    } catch {
+      console.log("------ G 1 --");
     }
     
+    try {
+            if(!s2) {
+              console.log('======= 2 ========');
+              var y = document.querySelector(".cart-page-footer__checkout button");
+              if(y) {
+                y.click();
+                s2 = true;
+              } else if(s1) {
+                if(c2>5) {
+                  s1 = false;
+                  c2=0;
+                } else {
+                  c2++;
+                }
+              }
+            }
+    } catch {
+      console.log("------ G 2 --");
+    }
+    
+
+    try {
+          if(!s3) {
+            console.log('======= 3 ========');
+            var z = document.querySelector("._1WpGLP button");
+            if(z) {
+              if(z.click()) {
+                console.log("------ SUCESS --");
+                s3 = true;
+                c3 = 0;
+              }
+            } else if(s2) {
+                if(c3>10) {
+                  s2 = false;
+                  c3=0;
+                } else {
+                  c3++;
+                }
+            }
+          }
+    } catch {
+      console.log("------ G 3 --");
+    }
+    
+    //close date if
   }
   
   
